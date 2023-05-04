901465946c3707cb695130a7a1cd18e5d99b4037
Phanesan <yahir.romo@hotmail.com>
2023-05-03 20:05:30 -0700

    Se agrega la base del proyecto

---
+++ src/main/java/WindowFrame.java
@@ -0,0 +1,42 @@
+package main.java;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import main.java.panel.MenuPanel;
+
+public class WindowFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WindowFrame frame = new WindowFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public WindowFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1280,720);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		MenuPanel menuPanel = new MenuPanel(0, 0);
+		contentPane.add(menuPanel, BorderLayout.CENTER);
+	}
+
+}
---
+++ src/main/java/panel/MenuPanel.java
@@ -0,0 +1,14 @@
+package main.java.panel;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class MenuPanel extends JPanel {
+
+	public MenuPanel(int width, int height) {
+		setSize(width,height);
+		setBackground(Color.DARK_GRAY);
+	}
+
+}
