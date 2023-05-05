d37835197eb164f264bd788d87e2b1274ac88457
Aleksey Unzon <rubenalekseyunzon@gmail.com>
2023-05-04 20:41:26 -0700

    Cree la clase Location y añadi constructores a Location y Entity

--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -1,7 +1,27 @@
 package main.java;
 
-public class Entity {
-
-	// Hello
+public class Entity{
+	
+	private boolean alive = false;
+	private Location location;
+	
+	public Entity(Location location) {
+		this.location = getLocation();
+		this.alive = isAlive();
+	}
+	
+	
+	public boolean isAlive() {
+		return alive;
+	}
+	
+	public boolean kill() {
+		return false;
+		
+	}
+	
+	public Location getLocation() {
+		return location;
+	}
 	
 }
---
+++ src/main/java/Location.java
@@ -0,0 +1,23 @@
+package main.java;
+
+public class Location {
+	
+	private int x, y;
+	
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public Location(int x, int y) {
+		this.x = 1;
+		this.y = 1;
+		x = this.x;
+		y = this.y;
+		
+	}
+	
+}
