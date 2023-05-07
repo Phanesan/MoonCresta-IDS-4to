f1a1d5e56a9f5d95f0c80f46f20625692f626b54
RubenJunco <128674160+RubenJunco@users.noreply.github.com>
2023-05-06 17:53:58 -0700

    cambios en el codigo de la imagen

--- src/main/java/panel/MenuPanel.java
+++ src/main/java/panel/MenuPanel.java
@@ -19,17 +19,22 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.border.Border;
+
+import main.java.utils.ImageUtil;
+
 import java.io.File;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
 public class MenuPanel extends JPanel  {
 	private Clip clip;
+	
 	public MenuPanel() {
-		setSize(1280,720);
-		setBackground(Color.GRAY);
 		
+		setSize(1280,720);
+		setBackground(new Color(0, 0, 0));
 		setLayout(null);
 		JPanel panel = new JPanel();
 		panel.setBounds(0, 0, 1280, 720);
@@ -45,8 +50,12 @@ public class MenuPanel extends JPanel  {
 		Empezar.setFont(new Font("Wide Latin", Font.PLAIN, 25));
 		Empezar.setBorder(border);
 		Empezar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					 
+				}
+			
 		});
 		panel.setLayout(null);
 		Empezar.setBounds(490, 301, 284, 55);
@@ -57,56 +66,78 @@ public class MenuPanel extends JPanel  {
 		Salir.setBorder(border);
 		Salir.setBackground(new Color(0, 0, 0));
 		Salir.setFont(new Font("Wide Latin", Font.PLAIN, 25));
-		Salir.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
 		Salir.setBounds(535, 405, 201, 55);
 		panel.add(Salir);
+		
+		Salir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		
 		
 		JLabel Titulo = new JLabel("New label");
 		Titulo.setForeground(new Color(64, 128, 128));
 		Titulo.setBackground(new Color(0, 128, 0));
 		Titulo.setBounds(430, 11, 400, 255);
-		Titulo.setIcon(new ImageIcon("Titulo.png"));
+		
+		Titulo.setIcon(new ImageIcon(ImageUtil.resizeImage(400, 228, ImageUtil.getStream("main/resources/Titulo.png"))));
 		panel.add(Titulo);
 		
-		JButton Musica = new JButton("M");
-		Musica.setFont(new Font("Tahoma", Font.BOLD, 26));
+		JButton Musica = new JButton("On");
+		Musica.setFont(new Font("Tahoma", Font.BOLD, 15));
 		Musica.setForeground(new Color(255, 255, 255));
 		Musica.setBorder(border);
 		Musica.setBackground(new Color(0, 0, 0));
 		Musica.setBounds(11, 516, 60, 60);
+		File audioFile = new File("Tema.wav");
+		
+		
+		
+		
+		try {
+            // Crear un objeto Clip y cargar el audio
+            clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(audioFile));
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    
+		
 		Musica.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent e) {
-				
-				 if (e.getSource() == Musica) {
-					 
-			            try {
-			                // Carga el archivo de audio
-			                File audioFile = new File("Tema.wav");
-			                clip = AudioSystem.getClip();
-			                clip.open(AudioSystem.getAudioInputStream(audioFile));
-
-			                // Reproduce el audio
-			                clip.start();
-
-			            } catch (Exception ex) {
-			                System.err.println("Error al reproducir audio: " + ex.getMessage());
-			            }
-			        }
+				if (e.getSource() == Musica) {
+		            if (clip.isActive()) {
+		                clip.stop();
+		              
+		                Musica.setText("off");
+		                
+		            } else {
+		                clip.loop(Clip.LOOP_CONTINUOUSLY);
+		                Musica.setText("On");
+		               
+		            }
+		        }
+			        
 			}
 		});
 		panel.add(Musica);
 		
 		
 		JLabel Fondo = new JLabel("");
-		Fondo.setForeground(new Color(64, 128, 128));
-		Fondo.setBackground(new Color(0, 128, 0));
+		Fondo.setFont(new Font("Wide Latin", Font.PLAIN, 25));
+		Fondo.setForeground(new Color(255, 255, 255));
+		Fondo.setBackground(new Color(255, 255, 255));
 		Fondo.setBounds(0, 0, 1280, 720);
 		Fondo.setIcon(new ImageIcon("Fondo.gif"));
 		panel.add(Fondo);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(11, 492, 46, 14);
+		panel.add(lblNewLabel);
 		
 		
 		
@@ -140,6 +171,4 @@ public class MenuPanel extends JPanel  {
 			
 		}  
 	}
-	
-
 }
