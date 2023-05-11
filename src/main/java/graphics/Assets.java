f2981273def42cc5a8770454123adcb99057b7fe
Phanesan <48665230+Phanesan@users.noreply.github.com>
2023-05-11 10:27:06 -0700

    implementado una pantalla de gameover

--- src/main/java/graphics/Assets.java
+++ src/main/java/graphics/Assets.java
@@ -12,6 +12,8 @@ public abstract class Assets {
 	public static final BufferedImage BACKGROUND_GAME = Loader.imageLoader("/main/resources/background_game.png");
 	public static final BufferedImage SHOT_ENEMY = Loader.imageLoader("/main/resources/DisparoEnemigo.png");
 	public static final BufferedImage ENEMY1 = Loader.imageLoader("/main/resources/enemy.png");
+	public static final BufferedImage BLACKSCREEN = Loader.imageLoader("/main/resources/BlackScreen.png");
+	
 	
 	// SONIDOS
 	
